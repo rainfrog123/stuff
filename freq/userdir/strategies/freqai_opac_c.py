5ff42e58c3f88f64f8f3b99b296edac3603e25a1
ffd17e99cad40cb6cd5620d7 <fe90ad073c68471cc605a43f@example.com>
2024-11-12 02:23:10 +0700

    update

--- freq/userdir/strategies/freqai_opac_c.py
+++ freq/userdir/strategies/freqai_opac_c.py
@@ -5,6 +5,7 @@ import talib.abstract as ta
 from pandas import DataFrame
 from technical import qtpylib
 from freqtrade.strategy import IStrategy
+import numpy as np
 
 logger = logging.getLogger(__name__)
 
@@ -24,7 +25,7 @@ class freqai_opaq_classifier(IStrategy):
     can_short = True
 
     stoploss = -0.005
-    # trailing_stop = True
+    trailing_stop = True
     # trailing_stop_positive = 0.005
 
     def feature_engineering_expand_all(
@@ -52,7 +53,6 @@ class freqai_opaq_classifier(IStrategy):
         dataframe["%-relative_volume-period"] = (
             dataframe["volume"] / dataframe["volume"].rolling(period).mean()
         )
-
         return dataframe
 
     def feature_engineering_expand_basic(
@@ -71,11 +71,11 @@ class freqai_opaq_classifier(IStrategy):
         return dataframe
 
     def set_freqai_targets(self, dataframe: DataFrame, metadata: dict, **kwargs) -> DataFrame:
-        # Set classification target: up, down, or same
+        # Set classification target: "up" or "down/same"
+        self.freqai.class_names = ["down", "up"]
         future_price = dataframe["close"].shift(-self.freqai_info["feature_parameters"]["label_period_candles"])
-        dataframe["&-class"] = (
-            future_price > dataframe["close"]
-        ).astype(int)  # 1 for up, 0 for down/same
+        dataframe["&-class"] = np.where(future_price > dataframe["close"], "up", "down")
+
         return dataframe
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
@@ -85,7 +85,7 @@ class freqai_opaq_classifier(IStrategy):
     def populate_entry_trend(self, df: DataFrame, metadata: dict) -> DataFrame:
         enter_long_conditions = [
             df["do_predict"] == 1,
-            df["&-class"] == 1,  # class 1 means up (predicted price increase)
+            df["&-class"] == 'up',  # class 1 means up (predicted price increase)
         ]
 
         if enter_long_conditions:
@@ -95,7 +95,7 @@ class freqai_opaq_classifier(IStrategy):
 
         enter_short_conditions = [
             df["do_predict"] == 1,
-            df["&-class"] == 0,  # class 0 means down (predicted price decrease)
+            df["&-class"] == 'down',  # class 0 means down (predicted price decrease)
         ]
 
         if enter_short_conditions:
@@ -106,6 +106,24 @@ class freqai_opaq_classifier(IStrategy):
         return df
 
     def populate_exit_trend(self, df: DataFrame, metadata: dict) -> DataFrame:
+    # Exit long position if the prediction changes to "down"
+        exit_long_conditions = [
+            df["do_predict"] == 1,
+            df["&-class"] == 'down'  # Exit long if prediction is "down"
+        ]
+        if exit_long_conditions:
+            df.loc[reduce(lambda x, y: x & y, exit_long_conditions), "exit_long"] = 1
+
+        # Exit short position if the prediction changes to "up"
+        exit_short_conditions = [
+            df["do_predict"] == 1,
+            df["&-class"] == 'up'  # Exit short if prediction is "up"
+        ]
+        if exit_short_conditions:
+            df.loc[reduce(lambda x, y: x & y, exit_short_conditions), "exit_short"] = 1
+
+        return df
+
         # exit_long_conditions = [df["do_predict"] == 1, df["&-class"] == 0]
         # if exit_long_conditions:
         #     df.loc[reduce(lambda x, y: x & y, exit_long_conditions), "exit_long"] = 1
